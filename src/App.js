fdfdaf4a68542b65406ccd1f213f1cc443bcfc74
Abhinay <makumar674@gmail.com>
2022-12-26 17:33:07 +0530

    jokesgenerator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    getJokes()
+  }, [])
+
+  const getJokes = async () => {
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await response.json();
+    setJoke(data.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='box' >
+      <h3>Chuck Norris Joke Generator</h3>
+      <p>{joke}</p>
+      <button onClick={getJokes} >Get new joke</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
